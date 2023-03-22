fe0c3de8fa6bb1ed8e15559b016bdf2d0c3a3097
smash-hq <smash_hq@outlook.com>
2023-03-22 10:22:17 +0800

    first

---
+++ src/test/java/com/smash/data/realtime/RealTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smash.data.realtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
